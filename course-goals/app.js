fb3d652d14f48fa15f082380bb40105977290439
Edward Hong <edward.hong527@gmail.com>
2021-11-03 12:58:53 +1300

    S02L05 Understanding "methods" in Vue Apps

--- course-goals/app.js
+++ course-goals/app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
 			vueLink: 'https://vuejs.org/',
 		}
 	},
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random()
+			if (randomNumber < 0.5) {
+				return 'Learn Vue!'
+			}
+			return 'Master Vue!'
+		},
+	},
 })
 
 app.mount('#user-goal')
